38b5d5d675e5747f58a2a6f430b8a69d502ae993
zeroencryptions <101370420+zeroencryptions@users.noreply.github.com>
2022-03-21 17:26:04 +0000

    Add files via upload

--- NodeLoaderV1.2a/calc.py
+++ NodeLoaderV1.2a/calc.py
@@ -4,6 +4,9 @@ def main():
     userOperation = input("what operation would you like?")
     userNo1 = int(input("what is your first number?"))
     userNo2 = int(input("and your second?"))
+    if userNo2 == 0:
+        print("nice try, but i don't want your computer to overheat")
+        return
     if userOperation == "+":
         answer = userNo1 + userNo2
         print ("answer is", answer)
