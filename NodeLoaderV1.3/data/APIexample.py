884b13852f7316a7e0819ac96bed0ebd099feaf3
zeroencryptions <101370420+zeroencryptions@users.noreply.github.com>
2022-04-30 15:48:43 +0100

    Add files via upload

---
+++ NodeLoaderV1.3/data/APIexample.py
@@ -0,0 +1,15 @@
+#NoΔΞ API Template v1.0
+#file template created by zencrypto
+
+#remember, all files need to be listed using the -a command in the node shell
+
+#keep the main() function and don't run it anywhere else in the file
+    #the node needs to read this for compatibility across all extentions
+
+#add whatever you like, but name the file accordingly and add a description
+
+#file name - template
+#description - template for user extentions 
+
+def main(): 
+    print("insert your code from this line")
---
+++ NodeLoaderV1.3/data/calc.py
@@ -0,0 +1,23 @@
+#file name - calculator
+#description - do calculations in seconds! 
+def main():
+    userOperation = input("what operation would you like?")
+    userNo1 = int(input("what is your first number?"))
+    userNo2 = int(input("and your second?"))
+    if userNo2 == 0:
+        print(userNo1)
+        return
+    if userOperation == "+":
+        answer = userNo1 + userNo2
+        print ("answer is", answer)
+    elif userOperation == "-":
+        answer = userNo1 - userNo2
+        print ("answer is", answer)
+    elif userOperation == "*":
+        answer = userNo1 * userNo2
+        print ("answer is", answer)
+    elif userOperation == "/":
+        answer = userNo1 / userNo2
+        print("answer is", answer)
+    else:
+        print("syntax error.")
